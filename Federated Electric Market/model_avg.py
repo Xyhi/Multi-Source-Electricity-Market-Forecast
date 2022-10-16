ec3c4c42571b27b6058bfec75bc604afde5adc3f
LJX <2840026059@qq.com>
2022-10-16 21:17:49 +0800

    保存不同参数对比值，frac已跑了1次

--- Federated Electric Market/model_avg.py
+++ Federated Electric Market/model_avg.py
@@ -11,6 +11,7 @@ import warnings
 import os
 warnings.filterwarnings('ignore')
 root_path = 'source_data/data'
+loss_type = ['MSE', 'MAE', 'RMSE']
 
 # 更新参数：根据输入的要改变的参数和列表，返回一个元素为args的列表
 def update_args(args,name,values):
@@ -26,40 +27,70 @@ def build_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)  # 如果不存在目录figure_save_path，则创建
 
-# 保存结果
-def save_result(loss_train, tol_test_loss, arg_name, arg_val):
-    # 创建文件夹
-    build_dir('./result_excel/train_loss/' + arg_name)
-    build_dir('./result_excel/test_loss/' + arg_name)
-    build_dir('./result_img/train_loss/' + arg_name)
-    build_dir('./result_img/test_loss/' + arg_name)
+# 保存训练集误差的表、图；测试集误差的表，图
+def save_data(loss_train, tol_test_loss,path1,path2,path3,path4):
 
     loss_train = np.array(loss_train)
     tol_test_loss = np.array(tol_test_loss)
 
-    loss_type = ['MSE', 'MAE', 'RMSE']
-    # 保存训练集各轮损失值
+    # 保存训练集误差
     loss_train_df = pd.DataFrame(loss_train)
     loss_train_df.columns = loss_type
-    loss_train_df.to_excel('./result_excel/train_loss/{}/{}={}.xlsx'.format(arg_name,arg_name,arg_val))
+    loss_train_df.to_excel(path1)
     # 绘图
     for i in range(3):
         plt.plot(range(len(loss_train)), loss_train[:,i])
         plt.ylabel('train_loss '+loss_type[i])
-        plt.savefig(
-            './result_img/train_loss/{}/{}={}-{}.png'.format(arg_name,arg_name,arg_val,loss_type[i]))
+        plt.savefig(path2+'-{}.png'.format(loss_type[i]))
         plt.show()
 
-    # 保存各客户端测试集损失值
+    # 保存测试集误差
     loss_test_df = pd.DataFrame(tol_test_loss)
     loss_test_df.columns = loss_type
-    loss_test_df.to_excel('./result_excel/test_loss/{}/{}={}.xlsx'.format(arg_name,arg_name,arg_val))
-
+    loss_test_df.to_excel(path3)
+    # 绘图
     for i in range(3):
         plt.boxplot(tol_test_loss[:,i])
-        plt.savefig(
-            './result_img/test_loss/{}/{}={}-{}.png'.format(arg_name,arg_name,arg_val,loss_type[i]))
+        plt.savefig(path4+'-{}.png'.format(loss_type[i]))
         plt.show()
+
+# 保存结果
+def save_result(loss_train, tol_test_loss, arg_name, arg_val):
+    # 创建文件夹
+    build_dir('./result_excel/train_loss/{}'.format(arg_name))
+    build_dir('./result_excel/test_loss/{}'.format(arg_name))
+    build_dir('./result_img/train_loss/{}'.format(arg_name))
+    build_dir('./result_img/test_loss/{}'.format(arg_name))
+    # 训练集误差的表、图；测试集误差的表，图  对应路径
+    path1 = './result_excel/train_loss/{}/{}={}.xlsx'.format(arg_name,arg_name,arg_val)
+    path2 = './result_img/train_loss/{}/{}={}'.format(arg_name,arg_name,arg_val)
+    path3 = './result_excel/test_loss/{}/{}={}.xlsx'.format(arg_name, arg_name, arg_val)
+    path4 = './result_img/test_loss/{}/{}={}'.format(arg_name, arg_name, arg_val)
+    # 保存
+    save_data(loss_train,tol_test_loss,path1,path2,path3,path4)
+
+    # # 保存训练集各轮损失值
+    # loss_train_df = pd.DataFrame(loss_train)
+    # loss_train_df.columns = loss_type
+    # loss_train_df.to_excel('./result_excel/train_loss/{}/{}={}.xlsx'.format(arg_name,arg_name,arg_val))
+    # # 绘图
+    # for i in range(3):
+    #     plt.plot(range(len(loss_train)), loss_train[:,i])
+    #     plt.ylabel('train_loss '+loss_type[i])
+    #     plt.savefig(
+    #         './result_img/train_loss/{}/{}={}-{}.png'.format(arg_name,arg_name,arg_val,loss_type[i]))
+    #     plt.show()
+    #
+    # # 保存各客户端测试集损失值
+    # loss_test_df = pd.DataFrame(tol_test_loss)
+    # loss_test_df.columns = loss_type
+    # loss_test_df.to_excel('./result_excel/test_loss/{}/{}={}.xlsx'.format(arg_name,arg_name,arg_val))
+    #
+    # for i in range(3):
+    #     plt.boxplot(tol_test_loss[:,i])
+    #     plt.savefig(
+    #         './result_img/test_loss/{}/{}={}-{}.png'.format(arg_name,arg_name,arg_val,loss_type[i]))
+    #     plt.show()
 
 # 调参过程中的模型训练和测试; arg_name, arg_val用于保存信息
 def train_test(server, arg_name, arg_val):
@@ -84,6 +115,7 @@ def train_test(server, arg_name, arg_val):
     # 保存结果
     save_result(loss_train, tol_test_loss, arg_name, arg_val)
 
+    return np.mean(loss_train,axis=0), np.mean(tol_test_loss,axis=0)
 
 if __name__ == '__main__':
 
@@ -99,14 +131,30 @@ if __name__ == '__main__':
 # 调参部分👆
 
 
-    # 训练并测试, 调参时可用for循环遍历args_list
+    # 遍历所有参数
     for name in all_args.keys():
+        # 创建目录用于保存结果
+        build_dir('./result_compare/{}/train'.format(name))
+        build_dir('./result_compare/{}/test'.format(name))
+
+        train_loss,test_loss = [], []
+        # 遍历参数的所有取值
         for i in all_args[name]:
             print('*******{}={}********'.format(name,i.__dict__[name]))
             # 服务端
-            Server = server(args)
+            Server = server(i)
             # 进入训练和测试
-            train_test(Server, name, i.__dict__[name])  # 传入服务端，调参的参数名，对应参数值；后两个参数用于保存信息
+            train_loss_temp, test_loss_temp = train_test(Server, name, i.__dict__[name])  # 传入服务端，调参的参数名，对应参数值；后两个参数用于保存信息
+            train_loss.append(train_loss_temp)
+            test_loss.append(test_loss_temp)
+        print('参数{}对应误差(mse,mae,rmse):\t训练集：{}\t测试集：{}'.format(name,train_loss,test_loss))
+        # 保存同一参数不同结果的对比
+        path1 = './result_compare/{}/train/{}.xlsx'.format(name,name)
+        path2 = './result_compare/{}/train/{}.png'.format(name, name)
+        path3 = './result_compare/{}/test/{}.xlsx'.format(name, name)
+        path4 = './result_compare/{}/test/{}.png'.format(name, name)
+        save_data(train_loss, test_loss, path1, path2, path3, path4)
+
 
 
 
